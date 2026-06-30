d1d6ab64fe51cadcd384a62e2a424f741da85a79
Maahina <kambojmaahina@gmail.com>
2026-06-30 21:56:02 +0530

    num of digits

--- digitcount.py
+++ digitcount.py
@@ -1,7 +1,7 @@
 '''enter a num and count its digits '''
 num=int(input("enter the num:"))
-x=0
+x=0   #count=x
 while num>0:
     x=x+1
-    num=num//10
-    print(f"Digit_count=",x)
+    num=num//10  #floor division--removes teh last integer everytime and resuls into num of digit
+    print(f"No of digits=",x)
